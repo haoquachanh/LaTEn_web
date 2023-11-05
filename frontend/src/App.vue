d7db6cb1dba72f7c488e183aab4ee8a53657868d
haoquachanh <anhhaomnmm@gmail.com>
2023-11-06 01:46:18 +0700

    st...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
+import { RouterView } from 'vue-router'
+import { provide,ref } from 'vue';
+import { defineAsyncComponent } from 'vue';
+import api from '@/api/index'; 
+
+const loggedIn = ref(false);
+const login = async (username: string, password: string) => {
+  try {
+    const response = await api.post('/login', { username, password });
+    if (response.data.success) {
+      loggedIn.value = true;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+provide('api', api);
+provide('loggedIn', loggedIn);
+provide('login', login);
+
+provide('api', api); 
 </script>
 
 <template>
