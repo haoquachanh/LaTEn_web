a9f2b92474bf98fc9f0db515c5fe2de231abc9de
haoquachanh <anhhaomnmm@gmail.com>
2024-01-12 23:53:09 +0700

    ~~

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 // /src/store/index.ts
 import { createStore } from "vuex";
 import auth from "./auth";
+import user from "./user";
 import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
   modules: {
     auth: auth,
+    user: user,
   },
   // plugins: [createPersistedState()],
 });
